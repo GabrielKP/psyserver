cf41adb53f14ce97b84699d6d98341fc41ae5299
Gabriel Kressin Palacios <gkressi1@jhu.edu>
2023-11-01 11:09:37 -0400

    csv saving

--- psyserver/main.py
+++ psyserver/main.py
@@ -1,9 +1,11 @@
+import csv
 import os
 import json
+from datetime import datetime
 from typing import Union, Dict, List
 from typing_extensions import Annotated
 
-from fastapi import Depends, FastAPI
+from fastapi import Depends, FastAPI, HTTPException
 from fastapi.staticfiles import StaticFiles
 from pydantic import BaseModel
 
@@ -15,26 +17,39 @@ class ExperimentData(BaseModel, extra="allow"):
     id: str
 
 
-class ExperimentDataCsv(BaseModel, extra="allow"):
+class ExperimentDataCsv(BaseModel):
     id: str
     trialdata: List[Dict] | None = None
+    fieldnames: List[str] | None = None
 
 
 app = FastAPI()
 
 
-# @app.post("/{experiment}/save/csv")
-# async def save_data(experiment: str, experiment_data: ExperimentDataCsv):
-#     id = experiment_data.id
-#     trialdata = experiment_data.trialdata
+@app.post("/{experiment}/save/csv")
+async def save_data(experiment: str, experiment_data: ExperimentDataCsv):
+    id = experiment_data.id
+    trialdata = experiment_data.trialdata
+    fieldnames = experiment_data.fieldnames
 
-#     data_dir = os.path.join("data", experiment)
-#     if not os.path.exists(data_dir):
-#         os.makedirs(data_dir)
+    data_dir = os.path.join("data", experiment)
+    if not os.path.exists(data_dir):
+        os.makedirs(data_dir)
 
-#     with open(os.path.join(data_dir, f"{id}.json"), "w") as f_out:
-#         json.dump(experiment_data, f_out)
-#     return {"status": "saved"}
+    now = str(datetime.now())[:19].replace(":", "-")
+
+    fieldnames = fieldnames or trialdata[0].keys()
+    with open(os.path.join(data_dir, f"{id}_{now}.json"), "w") as csvfile:
+        writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
+        writer.writeheader()
+        try:
+            for row in trialdata:
+                writer.writerow(row)
+        except ValueError as err:
+            error_msg = f"Trialdata contains non-specified key: {err}"
+            return HTTPException(status_code=422, detail=error_msg)
+
+    return {"status": "saved"}
 
 
 @app.post("/{experiment}/save")
@@ -49,7 +64,9 @@ async def save_data(
     if not os.path.exists(data_dir):
         os.makedirs(data_dir)
 
-    with open(os.path.join(data_dir, f"{id}.json"), "w") as f_out:
+    now = str(datetime.now())[:19].replace(":", "-")
+
+    with open(os.path.join(data_dir, f"{id}_{now}.json"), "w") as f_out:
         json.dump(dict(experiment_data), f_out)
     return {"status": "saved"}
 
