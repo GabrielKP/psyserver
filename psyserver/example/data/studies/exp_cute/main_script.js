d216aee0a996a2771e1686090532c0217a79f5da
Gabriel Kressin Palacios <gkressi1@jhu.edu>
2023-12-19 17:37:29 -0500

    saving: id->study_id, made study_id optional, removed save/csv route for simplicity

--- psyserver/example/data/studies/exp_cute/main_script.js
+++ psyserver/example/data/studies/exp_cute/main_script.js
@@ -103,7 +103,7 @@ function beginTrial() {
     // get final data
     DATA.endTime = Date.now();
     DATA.duration = performance.now();
-    DATA.id = DATA.prolificPID;
+    DATA.study_id = DATA.prolificPID;
     console.debug("saving data");
     // post data to server
     $.ajax({
